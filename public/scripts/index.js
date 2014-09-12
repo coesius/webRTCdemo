6166a20b4ef4feb920520c4392872639baaf223f
Andrew <coesius@qq.com>
2014-09-12 02:20:50 -0400

    some strange bug

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -13,5 +13,6 @@ rtc.on('add remote stream', function(stream){
 });
 
 // more rtc callbacks are available
-
-document.getElementById("local").volume = 0;
+document.onload = function() {
+  document.getElementById("local").volume = 0;
+}
