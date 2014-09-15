b04ebfad605bda8126f749ab82a18b24bb68d036
Andrew <coesius@qq.com>
2014-09-15 19:12:04 -0400

    try fix bug

--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,7 @@ router.get('/admin', function(req, res) {
 });
 
 router.get('/chat', function(req, res) {
-  res.render('admin');
+  res.render('admin', { title: 'webRTCdemo' });
 });
 
 module.exports = router;
